f47e6889d4956e51ac8879cf8d4d71dde50dbf95
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-19 22:59:02 -0400

    added score counter and next question

--- assets/script.js
+++ assets/script.js
@@ -11,13 +11,13 @@ const questionList = [
   },
   {
     question: "3. Arrays in Javascript can be used to store _____.",
-    answers: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-    correctAnswer: "all of the above",
+    answers: ['Numbers and Strings', 'Other Arrays', 'Booleans', 'All of the above'],
+    correctAnswer: "All of the above",
   },
   {
     question: "4. String values must be enclosed within ______ when being assigned to variables.",
-    answers: ['commas', 'brackets', 'quotes', 'parenthesis'],
-    correctAnswer: 'quotes',
+    answers: ['Commas', 'Brackets', 'Quotes', 'Parenthesis'],
+    correctAnswer: 'Quotes',
   },
   {
     question: "5. A very usefull tool used during developement and debugging for printing content to the debugger is: ______.",
@@ -28,32 +28,66 @@ const questionList = [
 var startBtnEl = document.getElementById("start-btn");
 var questionEl = document.getElementById("question");
 var btnListEl = document.querySelector(".btn-list");
-var answerButtonn1 = document.getElementById("btn-1");
-var answerButtonn2 = document.getElementById("btn-2");
-var answerButtonn3 = document.getElementById("btn-3");
-var answerButtonn4 = document.getElementById("btn-4");
+var answerButton1 = document.querySelector("#btn-1");
+var answerButton2 = document.querySelector("#btn-2");
+var answerButton3 = document.querySelector("#btn-3");
+var answerButton4 = document.querySelector("#btn-4");
 var nextBtnEl = document.getElementById("next-btn");
 var timerEl = document.getElementById("timer-count");
-var h2El = document.getElementById("question");
 var questionNum = 0;
-var correctAnswers = 0;
+var score = 0;
 
 var timerCount = 74;
 
+function answerSelect(event) {
+  checkSelectedAnswer(event);
+  questionNum++;
+  console.log(questionNum + 1);
+  if (questionNum > 4) {
+    console.log("quiz-has-ended");
+  }
+  else {
+    nextQuestion();
+  }
+  
+  
+}
 
-
-function loadQuestion() {
+function nextQuestion() {
   questionEl.textContent = questionList[questionNum].question;
-  answerButtonn1.textContent = questionList[questionNum].answers[0];
-  answerButtonn2.textContent = questionList[questionNum].answers[1];
-  answerButtonn3.textContent = questionList[questionNum].answers[2];
-  answerButtonn4.textContent = questionList[questionNum].answers[3];
+  answerButton1.textContent = questionList[questionNum].answers[0];
+  answerButton2.textContent = questionList[questionNum].answers[1];
+  answerButton3.textContent = questionList[questionNum].answers[2];
+  answerButton4.textContent = questionList[questionNum].answers[3];
+}
 
+function checkSelectedAnswer(event) {
+  if (event.target.textContent === questionList[questionNum].correctAnswer) {
+    score++;
+    console.log(score);
+  }
+  else {
+      if (timerCount < 10) {
+      timerCount = 0;
+    }
+    else {
+      timerCount -= 9;
+      timerEl.innerText = timerCount;
+    }
+  }
+}
 
-  answerButtons[0].addEventListener("click", console.log("hello"));
-  answerButton2.addEventListener("click", console.log("hello"));
-  answerButton3.addEventListener("click", console.log("hello"));
-  answerButton4.addEventListener("click", console.log("hello"));
+function endQuiz() {
+  
+}
+
+function loadQuestions() {
+  questionEl.textContent = questionList[questionNum].question;
+  answerButton1.textContent = questionList[questionNum].answers[0];
+  answerButton2.textContent = questionList[questionNum].answers[1];
+  answerButton3.textContent = questionList[questionNum].answers[2];
+  answerButton4.textContent = questionList[questionNum].answers[3];
+
 
   // for (var i = 0; i < answerButtons.length; i++) {
   //   answerButtons[i].addEventListener("click", function() {
@@ -67,9 +101,10 @@ function loadQuestion() {
 }
 
 function startTimer() {
-  timerEl.innerHTML = timerCount + 1;
+  timerEl.innerText = timerCount + 1;
+
   var timer = setInterval(function() {
-    timerEl.innerHTML = timerCount;
+    timerEl.innerText = timerCount;
     timerCount = timerCount - 1;
     if (timerCount < 0) {
       clearInterval(timer);
@@ -92,15 +127,20 @@ function startQuiz() {
   console.log("started");
   startBtnEl.classList.add("hide");
   btnListEl.classList.remove("hide");
-
-
+  
+  
   // answerBtnsEl.classList.remove("hide");
-  loadQuestion();
+  loadQuestions();
   startTimer();
 }
 
 
 startBtnEl.addEventListener("click", startQuiz);
+
+answerButton1.addEventListener("click", answerSelect);
+answerButton2.addEventListener("click", answerSelect);
+answerButton3.addEventListener("click", answerSelect);
+answerButton4.addEventListener("click", answerSelect);
 
 
 
