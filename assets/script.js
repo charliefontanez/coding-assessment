9ea5b30c8a9aa7450ef74195b98188aa1ca06d79
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-20 14:31:07 -0400

    added question counter to log questions. Setup highscore form

--- assets/script.js
+++ assets/script.js
@@ -38,20 +38,34 @@ var timerEl = document.getElementById("timer-count");
 var questionNum = 0;
 var score = 0;
 
+var scoreList = [];
+
+var userScore = {initial: "", score: 0};
+
 var timerCount = 74;
+
+function scoreScreen() {
+  scoreSubmitEl = document.querySelector(".your-score-screen");
+  quizContainerEl = document.querySelector(".quiz-container");
+  scoreDisplayEl = document.querySelector("#user-score-display");
+  scoreDisplayEl.textContent = score;
+  quizContainerEl.classList.add("hide");
+  scoreSubmitEl.classList.remove("hide");
+  timerCount = 0;
+  console.log("Done")
+  console.log("Your score is: " + score);
+}
+
 
 function answerSelect(event) {
   checkSelectedAnswer(event);
   questionNum++;
-  console.log(questionNum + 1);
   if (questionNum > 4) {
-    console.log("quiz-has-ended");
+    scoreScreen();
   }
   else {
-    nextQuestion();
-  }
-  
-  
+    loadQuestion();
+  }  
 }
 
 function nextQuestion() {
@@ -65,7 +79,6 @@ function nextQuestion() {
 function checkSelectedAnswer(event) {
   if (event.target.textContent === questionList[questionNum].correctAnswer) {
     score++;
-    console.log(score);
   }
   else {
       if (timerCount < 10) {
@@ -82,23 +95,13 @@ function endQuiz() {
   questionEl.textContent = "Your score is " + score;
 }
 
-function loadQuestions() {
+function loadQuestion() {
+  console.log((questionNum + 1) + '.');
   questionEl.textContent = questionList[questionNum].question;
   answerButton1.textContent = questionList[questionNum].answers[0];
   answerButton2.textContent = questionList[questionNum].answers[1];
   answerButton3.textContent = questionList[questionNum].answers[2];
   answerButton4.textContent = questionList[questionNum].answers[3];
-
-
-  // for (var i = 0; i < answerButtons.length; i++) {
-  //   answerButtons[i].addEventListener("click", function() {
-  //     if (this.textContent === questionList[questionNum].correctAnswer) {
-  //       correctAnswers++;
-  //       console.log("Correct answers = "+ correctAnswers);
-  //     }
-  //   });
-  // }
-
 }
 
 function startTimer() {
@@ -131,15 +134,27 @@ function startQuiz() {
   
   
   // answerBtnsEl.classList.remove("hide");
-  loadQuestions();
+  loadQuestion();
   startTimer();
 }
 
+function saveScore(e) {
+  e.preventDefault();
+  userScore.initial = document.getElementById("score-form-text").value;
+  userScore.score = score;
+  scoreList.push(userScore);
+  console.dir(userScore);
+  console.dir(scoreList[0]);
+}
+
+
+var saveScoreFormEl = document.getElementById("score-form");
+// var saveScoreTxt = document.getElementById("score-form-text");
+
+saveScoreFormEl.addEventListener("submit", saveScore)
+
 /* Menu Event Listeners */
 startBtnEl.addEventListener("click", startQuiz);
-viewHighScoresEl.addEventListener("click")
-
-
 /* Quiz Event Listeners */
 answerButton1.addEventListener("click", answerSelect);
 answerButton2.addEventListener("click", answerSelect);
