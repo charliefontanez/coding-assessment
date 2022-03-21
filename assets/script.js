c52d337b600a549b4689ebc62812c064792a26c1
Carlos Fontanez <charliefontanezis@gmail.com>
2022-03-20 23:47:29 -0400

    added high score page, score submit page and high score generation from local storage

--- assets/script.js
+++ assets/script.js
@@ -26,8 +26,8 @@ const questionList = [
   }
 ]
 var startBtnEl = document.getElementById("start-btn");
-var viewHighScoresEl = document.querySelector("view-highscore");
-var questionEl = document.getElementById("question");
+var viewHighScores = document.querySelector(".view-highscore");
+var h2El = document.getElementById("question");
 var btnListEl = document.querySelector(".btn-list");
 var answerButton1 = document.querySelector("#btn-1");
 var answerButton2 = document.querySelector("#btn-2");
@@ -35,28 +35,41 @@ var answerButton3 = document.querySelector("#btn-3");
 var answerButton4 = document.querySelector("#btn-4");
 var nextBtnEl = document.getElementById("next-btn");
 var timerEl = document.getElementById("timer-count");
+
+/* Score Screen Selectors */
+const yourScoreScreenEl = document.getElementById("#your-score-screen");
+var quizEl = document.querySelector(".quiz-container");
+var yourScoreEl = document.querySelector("#user-score-display");
+var scoreListEl = document.getElementById("score-list");
+var saveScoreFormEl = document.getElementById("your-score-screen");
+
+saveScoreFormEl.style.display = "none";
+
 var questionNum = 0;
 var score = 0;
 
-var scoreList = [];
+
+
+var scoreList = JSON.parse(window.localStorage.getItem('highscores')) || [];
+
+
 
 var userScore = {initial: "", score: 0};
 
+
 var timerCount = 74;
 
-function scoreScreen() {
-  scoreSubmitEl = document.querySelector(".your-score-screen");
-  quizContainerEl = document.querySelector(".quiz-container");
-  scoreDisplayEl = document.querySelector("#user-score-display");
-  scoreDisplayEl.textContent = score;
-  quizContainerEl.classList.add("hide");
-  scoreSubmitEl.classList.remove("hide");
-  timerCount = 0;
-  console.log("Done")
-  console.log("Your score is: " + score);
+
+/* Initial Quiz Functions */
+function startQuiz() {
+  console.log("started");
+  startBtnEl.classList.add("hide");
+  btnListEl.classList.remove("hide");
+  loadQuestion();
+  startTimer();
 }
 
-
+/* Quiz answer section */
 function answerSelect(event) {
   checkSelectedAnswer(event);
   questionNum++;
@@ -69,7 +82,7 @@ function answerSelect(event) {
 }
 
 function nextQuestion() {
-  questionEl.textContent = questionList[questionNum].question;
+  h2El.textContent = questionList[questionNum].question;
   answerButton1.textContent = questionList[questionNum].answers[0];
   answerButton2.textContent = questionList[questionNum].answers[1];
   answerButton3.textContent = questionList[questionNum].answers[2];
@@ -91,17 +104,24 @@ function checkSelectedAnswer(event) {
   }
 }
 
-function endQuiz() {
-  questionEl.textContent = "Your score is " + score;
-}
-
 function loadQuestion() {
   console.log((questionNum + 1) + '.');
-  questionEl.textContent = questionList[questionNum].question;
+  h2El.textContent = questionList[questionNum].question;
   answerButton1.textContent = questionList[questionNum].answers[0];
   answerButton2.textContent = questionList[questionNum].answers[1];
   answerButton3.textContent = questionList[questionNum].answers[2];
   answerButton4.textContent = questionList[questionNum].answers[3];
+}
+/* End Quiz answer Section */
+
+function scoreScreen() {
+  yourScoreEl.textContent = score;
+  quizEl.style.display = "none";
+  let yourScoreScreen = document.getElementById("your-score-screen");
+  yourScoreScreen.style.display = "";
+  timerCount = 0;
+  console.log("Done")
+  console.log("Your score is: " + score);
 }
 
 function startTimer() {
@@ -113,48 +133,60 @@ function startTimer() {
     if (timerCount < 0) {
       clearInterval(timer);
     }
-  }, 1000);
+  }
+  , 1000);
 }
 
-// function showHideButtonAnswers() {
-//   for (var i = 0; i < answerBtnsEl.classList.length; i++) {
-//     if (answerBtnsEl.classList[i] == "hide") {
-//       answerBtnsEl.classList.remove("hide");
-//     }
-//     else if (i == answerBtnsEl.classList.length - 1) {
-//       answerBtnsEl.classList.add("hide");
-//     }
-//   }
-// }
+function showScores(e) {
+  
+  for (var i = 0; i < scoreList.length; i++) {
+    let newScore = document.createElement("li");
+    newScore.appendChild(document.createTextNode(scoreList[i].initial +  " " + scoreList[i].score + "/5"));
+    console.dir(newScore);
+    scoreListEl.appendChild(newScore);
+  }
 
-function startQuiz() {
-  console.log("started");
-  startBtnEl.classList.add("hide");
-  btnListEl.classList.remove("hide");
+  document.querySelector("#highscores-page").style.display = "block";
+
+  if (quizEl.style.display == "") {
+    quizEl.style.display = "none";
+  }
+  if (yourScoreEl.style.display == "") {
+    yourScoreEl.style.display == "none";
+  }
   
-  
-  // answerBtnsEl.classList.remove("hide");
-  loadQuestion();
-  startTimer();
+
 }
 
 function saveScore(e) {
   e.preventDefault();
+
   userScore.initial = document.getElementById("score-form-text").value;
   userScore.score = score;
   scoreList.push(userScore);
-  console.dir(userScore);
-  console.dir(scoreList[0]);
+  console.log(scoreList);
+
+  window.localStorage.setItem('highscores', JSON.stringify(scoreList));
+
+  h2El.textContent = "Coding Quiz.";
+
+  e.target.parentNode.style.display = "none";
+  startBtnEl.style.display = "block";
+  btnListEl.style.display = "none";
+  quizEl.style.display = "";
+
+  timerCount = 74;
+  questionNum = 0;
 }
 
-
-var saveScoreFormEl = document.getElementById("score-form");
 // var saveScoreTxt = document.getElementById("score-form-text");
 
-saveScoreFormEl.addEventListener("submit", saveScore)
+
 
 /* Menu Event Listeners */
 startBtnEl.addEventListener("click", startQuiz);
+viewHighScores.addEventListener("click", showScores);
+saveScoreFormEl.addEventListener("submit", saveScore)
 /* Quiz Event Listeners */
 answerButton1.addEventListener("click", answerSelect);
 answerButton2.addEventListener("click", answerSelect);
